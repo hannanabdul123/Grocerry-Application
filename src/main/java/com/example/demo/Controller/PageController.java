32708f629e8ac099d82bc775b1af784996d779a2
Abdul Hannan <abdhannan121@gmail.com>
2026-04-28 20:52:15 +0530

    New Commit

--- src/main/java/com/example/demo/Controller/PageController.java
+++ src/main/java/com/example/demo/Controller/PageController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class PageController {
 
-    @GetMapping("/")
+@GetMapping("/")
 public String homePage() {
     return "home";
 }
